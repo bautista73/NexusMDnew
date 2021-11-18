36b194731031fc8f593ad36e3db5111bc07cc8b9
bbautista73 <bbautista73@gmail.com>
2021-11-18 15:04:53 -0600

    Fixed

--- NexusMD.MVC/App_Start/BundleConfig.cs
+++ NexusMD.MVC/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace NexusMD.MVC
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,17 +13,28 @@ namespace NexusMD.MVC
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/adminlte/css/adminlte.min.css",
+                      "~/adminlte/plugins/fullcalendar/main.min.css",
+                      "~/adminlte/plugins/fullcalendar-daygrid/main.min.css",
+                      "~/adminlte/plugins/fullcalendar-bootstrap/main.min.css",
+                      "~/adminlte/plugins/fontawesome-free/css/all.min.css",
+                      "~/Content/site.css"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/adminlte/js").Include(
+                       "~/adminlte/js/adminlte.min.js",
+                       "~/adminlte/plugins/fullcalendar/main.js",
+                       "~/adminlte/plugins/fullcalendar-daygrid/main.js",
+                       "~/adminlte/plugins/fullcalendar-bootstrap/main.js"
+                       ));
         }
     }
 }
